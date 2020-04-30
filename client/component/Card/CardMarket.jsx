4d50100ccf12a337f290eee85b988738f62b8aab
flo071 <flo071@gmx.at>
2020-04-30 16:53:11 +0200

    switch cmc to coingecko

--- client/component/Card/CardMarket.jsx
+++ client/component/Card/CardMarket.jsx
@@ -60,7 +60,7 @@ export default class CardStatus extends Component {
                 </span>
                 <span>In {this.props.xAxis.length * 5} minutes</span>
               </p>
-              <p className="card__info-source">Data from CoinMarketCap</p>
+              <p className="card__info-source">Data from CoinGecko</p>
             </div>
             <div className="col-sm-12 col-md-6 col-lg-8">
               <GraphLine
