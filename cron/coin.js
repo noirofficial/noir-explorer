03fcd5f0e3e691db614dbbb1bd016b4e747ae0f9
flo071 <flo071@gmx.at>
2020-04-30 16:48:48 +0200

    switch cmc to coingecko

--- cron/coin.js
+++ cron/coin.js
@@ -31,27 +31,28 @@ async function syncCoin() {
   const nethashps = await rpc.call('getnetworkhashps');
 
   let market = await fetch(url);
-  if (Array.isArray(market)) {
-    market = market.length ? market[0] : {};
-  }
+  //if (Array.isArray(market)) {
+  //  market = market.length ? market[0] : {};
+  //  console.log(market);
+  //}
 
   const countCarverAddresses = await CarverAddress.find({ carverAddressType: CarverAddressType.Address }).count();
   const countCarverMovements = await CarverMovement.find({ isReward: false }).count();
 
 
   const coin = new Coin({
-    cap: market.market_cap_usd,
+    cap: market.market_data.market_cap.usd,
     createdAt: date,
     blocks: info.blocks,
-    btc: market.price_btc,
+    btc: market.market_data.current_price.btc,
     diff: info.difficulty,
     mnsOff: masternodes_all - masternodes_en,
     mnsOn: masternodes_all,
     netHash: nethashps,
     peers: info.connections,
     status: 'Online',
-    supply: market.available_supply, // TODO: change to actual count from db.
-    usd: market.price_usd,
+    supply: market.market_data.circulating_supply, // TODO: change to actual count from db.
+    usd: market.market_data.current_price.usd,
     countCarverAddresses,
     countCarverMovements
   });
